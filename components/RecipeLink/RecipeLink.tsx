c079f7dbc967755ed35272de08583498564db87b
Tim Copland <tim.copland@august.com.au>
2023-12-26 12:47:55 +1000

    Add arrows to links

--- components/RecipeLink/RecipeLink.tsx
+++ components/RecipeLink/RecipeLink.tsx
@@ -1,6 +1,7 @@
 import { FC, HTMLProps } from "react";
 import ListItemButton from '@mui/material/ListItemButton';
 import ListItemText from '@mui/material/ListItemText';
+import ArrowForward from '@mui/icons-material/ArrowForward';
 
 export type RecipeLinkType = {
     name: string;
@@ -17,8 +18,9 @@ export const RecipeLink: FC<Props> = ({
     onClick,
 }: Props) => {
     return (
-        <ListItemButton component="button" onClick={onClick}>
+        <ListItemButton component="button" onClick={onClick} sx={{ gap: '0.5rem' }}>
             <ListItemText primary={label} />
+            <ArrowForward />
         </ListItemButton>
     )
 }
